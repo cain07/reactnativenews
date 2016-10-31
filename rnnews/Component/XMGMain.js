5e858b9b8a224fbcd3a0066c5748280b96500ad9
yaojianwei <yaojianwei@meicai.cn>
2016-10-31 19:32:58 +0800

    1

--- rnnews/Component/XMGMain.js
+++ rnnews/Component/XMGMain.js
@@ -17,6 +17,13 @@ var Message = require('../Component/XMGMessage');
 var Mine = require('../Component/XMGMine');
 
 var Main = React.createClass({
+    getDefaultProps(){
+        return{
+           url_api:"http://c1.m.163.com/nc/article/headline/T1348647853363/0-20.html?from=toutiao&fn=2&passport=&devId=nTM86EPlcxZu09VdpTEh6aR3%2B%2FQX6x8vHBD3ne3k5bbgOrg%2FIP5DcguSDmtYyWbs&size=20&version=8.1&spever=false&net=wifi&lat=5OtqEKiivwW4K%2BGMt6DBdA%3D%3D&lon=jKlRVyYkSNti2wwsjGQHrw%3D%3D&ts=1463384311&sign=TtD7IZllDljVzBs2E4sa9fQyKTKF021w2EUC6qx1gEN48ErR02zJ6%2FKXOnxX046I&encryption=1&canal=appstore",
+            key_word:'T1348647853363'
+        }
+    },
+
     getInitialState(){
         return{
             selectedItem:'home'
