061bb0a550e7650cc11b36f3f8f3f104381bd06c
yaojianwei <yaojianwei@meicai.cn>
2016-10-31 14:23:38 +0800

    各个页面

--- rnnews/Component/XMGMain.js
+++ rnnews/Component/XMGMain.js
@@ -7,24 +7,93 @@ import {
     StyleSheet,
     Text,
     View,
-    TabBarIOS
+    TabBarIOS,
+    NavigatorIOS
 } from 'react-native';
 
+var Home = require('../Component/XMGHome');
+var Find = require('../Component/XMGFind');
+var Message = require('../Component/XMGMessage');
+var Mine = require('../Component/XMGMine');
 
 var Main = React.createClass({
+    getInitialState(){
+        return{
+            selectedItem:'home'
+        }
+    },
+
     render(){
         return (
-            <View style={styles.container}>
               <TabBarIOS tintColor="orange">
                   <TabBarIOS.Item
-                  icon = {require('image!')}
+                  icon = {require('image!tabbar_home')}
                   title="首页"
-
+                  selected={this.state.selectedItem == 'home'}
+                  onPress={()=>{this.setState({selectedItem:'home'})}}
                   >
+                    <NavigatorIOS
+                        style={{flex:1}}
+                       initialRoute ={
+                          {
+                             component:Home,
+                             title:'网易'
+                          }
+                       }
+                    />
+                  </TabBarIOS.Item>
 
+                  <TabBarIOS.Item
+                      icon = {require('image!tabbar_discover')}
+                      title="发现"
+                      selected={this.state.selectedItem == 'discover'}
+                      onPress={()=>{this.setState({selectedItem:'discover'})}}
+                  >
+                      <NavigatorIOS
+                          style={{flex:1}}
+                          initialRoute ={
+                          {
+                             component:Find,
+                             title:'发现'
+                          }
+                       }
+                      />
+                  </TabBarIOS.Item>
+
+                  <TabBarIOS.Item
+                      icon = {require('image!tabbar_message_center')}
+                      title="消息"
+                      selected={this.state.selectedItem == 'message'}
+                      onPress={()=>{this.setState({selectedItem:'message'})}}
+                  >
+                      <NavigatorIOS
+                          style={{flex:1}}
+                          initialRoute ={
+                          {
+                             component:Message,
+                             title:'消息'
+                          }
+                       }
+                      />
+                  </TabBarIOS.Item>
+
+                  <TabBarIOS.Item
+                      icon = {require('image!tabbar_profile')}
+                      title="我的"
+                      selected={this.state.selectedItem == 'profile'}
+                      onPress={()=>{this.setState({selectedItem:'profile'})}}
+                  >
+                      <NavigatorIOS
+                          style={{flex:1}}
+                          initialRoute ={
+                          {
+                             component:Mine,
+                             title:'我的'
+                          }
+                       }
+                      />
                   </TabBarIOS.Item>
               </TabBarIOS>
-            </View>
         );
     }
 });
